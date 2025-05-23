f4e6871a2b562fad2c93c86b65fb119b98723aa2
Francisco Kaique <fkaiquesp@gmail.com>
2025-05-23 00:54:49 -0300

    Calculadora simples funcionando

--- src/index.js
+++ src/index.js
@@ -9,3 +9,90 @@ function mudarTema(antigo, novo){
         body.classList.remove("tema--escuro")
     }
 }
+//calculadora
+let a;
+let oper=''
+
+const tela = document.getElementById("tela");
+document.querySelector(".numeros").addEventListener('click', (event)=>{
+  if (!event.target.dataset.key){
+    return
+  }
+const value = event.target.dataset.key
+    if (value === '='){
+      console.log(a, oper)
+      switch (oper){
+        case '/':
+          a/=Number(tela.value)
+          tela.value = a;
+          break;
+        case '*':
+          a*=Number(tela.value)
+          tela.value = a;
+          break;
+        case '-':
+          a-=Number(tela.value)
+          tela.value = a;
+          break;
+        case '+':
+          a+=Number(tela.value)
+          tela.value = a;
+          break;
+      }
+    }else if (!isFinite(value)){
+      if (value==='-' && tela.value===""){
+        tela.value+='-'
+      }else if (value!== '.'){
+        a=Number(tela.value);
+        tela.value=null
+      }else if (tela.value!==""){
+        tela.value+=value
+      }
+      if (value!='.'){
+        oper=value;
+      }
+    }else{
+      tela.value += value;
+    }
+  console.log(event.target.dataset.key)
+})
+/*
+document.querySelectorAll('button').forEach(botao => {
+  botao.addEventListener('click', function () {
+    const tela = document.getElementById("tela");
+    
+    if (this.value === '='){
+      console.log(a, oper)
+      switch (oper){
+        case '/':
+          a/=Number(tela.value)
+          tela.value = a;
+          break;
+        case '*':
+          a*=Number(tela.value)
+          tela.value = a;
+          break;
+        case '-':
+          a-=Number(tela.value)
+          tela.value = a;
+          break;
+        case '+':
+          a+=Number(tela.value)
+          tela.value = a;
+          break;
+        case '=':
+          a=a
+          tela.value = a;
+          break;
+      }
+    }else if (!isFinite(this.value)){
+      oper=this.value;
+      tela.value=null
+    }else{
+      tela.value += this.value;
+    }
+    console.log(this.value);
+    
+  });
+});
+*/
