fbc5d5c82ed7085a22f5b6a40dd358818c3ad3cb
Fkaique <fkaiquesp@gmail.com>
2026-03-19 02:02:12 -0300

    fix

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -29,30 +29,33 @@ onUnmounted(() => {
 
 </script>
 <template>
-    <nav class="navbar">
-        <div class="linkMain">
-            <router-link class="link" to="/">Galeria</router-link>
-            <router-link class="link" to="/">Dormitório</router-link>
-        </div>
-        <div class="col-2">
-
-            <router-link class="navHome" to="/">Home</router-link>
-        </div>
-        <div class="col-3">
-            <div class="mudar-tema" @click="mudarTema"></div>
-            <div class="mudar-tema-hamburguer" @click="openBurguer" />
-            <div class="container-menu-burguer" @click.self="closeBurguer">
-                <div v-if="isVisible" class="menu-burguer" @click.stop>
-                    <div v-if="isVisible" class="mudar-tema-sus" @click="mudarTema">Mudar Tema</div>
-                    <li v-if="isVisible"><router-link class="link-sus" to="/">Dormitório</router-link></li>
-                    <li v-if="isVisible"><router-link class="link-sus" to="/">Galeria</router-link></li>
-                </div>
+    <div class="header-wrapper">
+        <nav class="navbar">
+            <div class="linkMain">
+                <router-link class="link" to="/">Galeria</router-link>
+                <router-link class="link" to="/">Dormitório</router-link>
             </div>
 
+            <div class="col-2">
+                <router-link class="navHome" to="/">Home</router-link>
+            </div>
+
+            <div class="col-3">
+                <div class="mudar-tema" @click="mudarTema"></div>
+                <div class="mudar-tema-hamburguer" @click.stop="openBurguer"></div>
+            </div>
+        </nav>
+
+        <div v-if="isVisible" class="menu-burguer" @click.stop>
+            <div class="mudar-tema-sus" @click="mudarTema">Mudar Tema</div>
+            <router-link class="link-sus" to="/">Dormitório</router-link>
+            <router-link class="link-sus" to="/">Galeria</router-link>
         </div>
-    </nav>
+    </div>
 </template>
 <style>
+
+
 .mudar-tema,
 .mudar-tema-hamburguer {
     width: 50px;
@@ -65,17 +68,20 @@ onUnmounted(() => {
     position: relative;
 }
 
+.header-wrapper {
+    position: relative;
+}
 
 .menu-burguer {
     position: absolute;
-    top: 50px;
+    top: 80px;
+    right: 20px;
     padding: 10px;
-    right: 0;
     display: flex;
     flex-direction: column;
-    justify-content: start;
-    align-items: start;
     background-color: rgb(132, 128, 128);
+    z-index: 1000;
+    color: var(--color-text-contrast);
 }
 
 .link,
@@ -121,12 +127,10 @@ onUnmounted(() => {
     padding: 20px;
     background-image: linear-gradient(to bottom, var(--color-text-contrast), rgba(0, 0, 0, 0));
     position: relative;
-    overflow: hidden;
 }
 
-.navbar >* {
+.navbar>* {
     flex: 1;
-    overflow: hidden;
 }
 
 .navbar .col-3 {
