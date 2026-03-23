ba412c758fdbfc1437fbe5daaf0c9af17c4e7d16
Fkaique <fkaiquesp@gmail.com>
2026-03-23 01:26:03 -0300

    fix: HashHistory

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHashHistory } from "vue-router"
 import DefaultLayout from "../defaultLayout.vue"
 import Home from "../pages/Home.vue"
 import Dormitorio from "../pages/Dormitorio.vue"
@@ -17,7 +17,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
 })
 
